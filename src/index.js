0781a0033ac9c20c23306f19042367705f1211be
dimeji54 <oladimejiabdulsalam54@gmail.com>
2023-10-27 19:28:38 -0400

    babam

--- src/index.js
+++ src/index.js
@@ -4,14 +4,13 @@ import App from "./App";
 import { persistor, store } from "./redux/store";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import Cart from "./components/Cart/Cart";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={"loading"} persistor={persistor}>
-        <Cart />
+        <App />
       </PersistGate>
     </Provider>
   </React.StrictMode>
